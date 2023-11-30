4842fcb62584511d97dee385a9b6b239e58f37d2
FranciscoN11 <fnavarrete@voxpop.com.mx>
2023-11-30 15:09:35 -0600

    first commit

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const port = 3001;
 
 app.use(bodyParser.json());
 
-// app.use(cors({
-//   origin: 'https://verificacfdi-front.vercel.app',
-//   methods: ['GET', 'POST'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true, // Habilita el intercambio de cookies entre dominios
-//   optionsSuccessStatus: 204, // Algunos navegadores pueden enviar una solicitud 'preflight' OPTIONS antes de la solicitud real, esta es la respuesta exitosa para la solicitud 'preflight'
-// }));
+app.use(cors({
+  origin: 'https://verificacfdi-front.vercel.app',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // Habilita el intercambio de cookies entre dominios
+  optionsSuccessStatus: 204, // Algunos navegadores pueden enviar una solicitud 'preflight' OPTIONS antes de la solicitud real, esta es la respuesta exitosa para la solicitud 'preflight'
+}));
 app.options('*', cors());
 
 mongoose.connect('mongodb+srv://verificacfdi:i5YJgDpCQU5HAVIS@cluster0.fttxxbj.mongodb.net/');
